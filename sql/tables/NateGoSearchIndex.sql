17f8a3860ff2eb1629f1c6b7209a0b25422d30ce
Michael Gauthier <mike@silverorange.com>
2006-05-02 16:04:49 +0000

    Change tag to document_type and added an index on document id.
    git-svn-id: file:///Users/gauthierm/svnrepo/nategosearch/trunk@5190 94490950-a86c-4493-9ccb-530f72933e47

--- sql/tables/NateGoSearchIndex.sql
+++ sql/tables/NateGoSearchIndex.sql
@@ -3,8 +3,9 @@ create table NateGoSearchIndex (
 	word varchar(32) not null,
 	weight integer not null,
 	location integer not null,
-	tag integer not null
+	document_type integer not null
 );
 
-create index NateGoSearchIndex_document_id_index on NateGoSearchIndex(tag);
+create index NateGoSearchIndex_document_id_index on NateGoSearchIndex(document_id);
+create index NateGoSearchIndex_document_type_index on NateGoSearchIndex(document_type);
 create index NateGoSearchIndex_word_index on NateGoSearchIndex(word);
