b19b6c8d34ec7ecc67dbee228cc4a7ef62f2e527
David Peck <dave@silverorange.com>
2006-08-15 16:22:54 +0000

    simplify create scripts
    git-svn-id: file:///Users/gauthierm/svnrepo/nategosearch/trunk@9627 94490950-a86c-4493-9ccb-530f72933e47

--- sql/create_sql.sh
+++ sql/create_sql.sh
@@ -1,21 +1,28 @@
 #!/bin/sh
 
-SRC="/so/packages/nate-go-search/work-dave/sql"
+WHOAMI=`whoami`
+WD="/so/packages/nate-go-search/work-${WHOAMI}/sql"
+DST="${WD}/statements.sql"
 
 if [ -z $1 ]; then
-    echo "need destination db name"
-    exit 0
+	echo "need destination db name"
+	exit 0
 else
-    DB=$1
+	DB=$1
 fi
 
-clear
-echo "Database: $1"
-echo
-echo
+# Create an empty site.
+echo "" > $DST
 
-${SRC}/generate_sql.sh
+cat ${WD}/tables/tables.txt | while read LINE; do
+	cat ${WD}/tables/${LINE} >> $DST
+done
 
-psql -U php -f ${SRC}/statements.sql $DB
-rm ${SRC}/statements.sql
+cat ${WD}/functions/functions.txt | while read LINE; do
+	cat ${WD}/functions/${LINE} >> $DST
+done
+
+echo "Creating the generic search tables"
+psql -U php -f $DST $DB
+rm $DST
 
